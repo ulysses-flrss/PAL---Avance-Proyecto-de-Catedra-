2e5ea699d173552e1ac53fbbe1a756cdb614ffb7
ulysses-flrss <ulisesf3136@gmail.com>
2022-03-28 16:47:15 -0600

    update

---
+++ avance.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void smsBienvenida();
+
+int main () {
+    smsBienvenida();
+    cout<<"1) \n";
+    cout<<"2) \n";
+    cout<<"3) \n";
+    cout<<"4) \n";
+    cout<<"5) \n";
+    cout<<"6) \n";
+}
+
+void smsBienvenida () {
+    cout<<"\t\t Sistema de Control de Inventarios\n";
+    cout<<"\t\tEmpresa Salesiana María Auxiliadora\n";
+    cout<<"\t\t      Versión del Sistema 1.0\n";
+    cout<<"\t\t        Techno Developers\n";
+}
