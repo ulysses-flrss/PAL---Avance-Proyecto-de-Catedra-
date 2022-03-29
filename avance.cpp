4d6c98a2130282caaba1888b986e9b8c88ce9d0c
Guillermo-31 <guillermogr211@gmail.com>
2022-03-28 23:03:15 -0600

    Actualización

--- avance.cpp
+++ avance.cpp
@@ -1,49 +1,71 @@
 #include <iostream>
 using namespace std;
 void smsBienvenida();
+int menuP();
 
 int main () {
+	setlocale(LC_ALL, "spanish");
+    bool opcionV = true;
     smsBienvenida();
-    cout<<"1)Sucursal \n";
-    cout<<"2)Proveedores \n";
-    cout<<"3)ArtÃ­culos \n";
-    cout<<"4)Movimientos artÃ­culos \n";
-    cout<<"5)Reportes \n";
-    cout<<"6) \n";
+	
+	do{
+		switch (menuP()){
 
-    int menu;
-     
-	 cout<<"Ingrese un numero del menu"<<endl;
-	 cin>>menu;
+        case 1:
+			cout<<"Has seleccionado Sucursal"<<endl;
+			opcionV = true;
+            break;
 
-        switch (menu){
-           case 1:
-             cout<<"Has seleccionado Sucursal";
-        break;
-            case 2:
-            cout<<"Has seleccionado Proveedores";
-        break;
-            case 3:
-            cout<<"Has seleccionado ArtÃ­culos";
-        break;
-            case 4:
-            cout<<"Has seleccionado Movimientos artÃ­culados";
-        break;
-            case 5:
-           cout<<"Has seleccionado Reportes";
-        break;
+        case 2:
+            cout<<"Has seleccionado Proveedores"<<endl;
+			opcionV = true;
+            break;
+
+        case 3:
+            cout<<"Has seleccionado ArtÃ­culos"<<endl;
+			opcionV = true;
+            break;
+
+        case 4:
+            cout<<"Has seleccionado Movimientos artÃ­culados"<<endl;
+			opcionV = true;
+            break;
+
+        case 5:
+    		cout<<"Has seleccionado Reportes"<<endl;
+			opcionV = true;
+            break; 
+
+        case 6:
+    		cout<<"Has seleccionado Salir"<<endl;
+			opcionV = true;
+            break;
     
-           default:
-           	cout<<"Ingrese una opcion correcta";
-        break;
-        }
+        default:
+            cout<<"Error. Ha ingresado una opción incorrecta. Inténtelo de nuevo";
+			opcionV = false;
+            break;
+    	}
+	} while(opcionV == false);
 }
-
-
 
 void smsBienvenida () {
     cout<<"\t\t Sistema de Control de Inventarios\n";
-    cout<<"\t\tEmpresa Salesiana MarÃ­a Auxiliadora\n";
-    cout<<"\t\t      VersiÃ³n del Sistema 1.0\n";
+    cout<<"\t\tEmpresa Salesiana María Auxiliadora\n";
+    cout<<"\t\t      Versión del Sistema 1.0\n";
     cout<<"\t\t        Techno Developers\n";
+}
+
+int menuP (){
+    int menuOpc;
+	cout<<"1)   Sucursal \n";
+    cout<<"2)   Proveedores \n";
+    cout<<"3)   Artículos \n";
+    cout<<"4)   Movimientos artículos \n";
+    cout<<"5)   Reportes \n";
+    cout<<"6)   Salir\n";     
+	
+	cout<<"Ingrese un numero del menu"<<endl;
+	cin>>menuOpc;
+	return menuOpc;
 }
