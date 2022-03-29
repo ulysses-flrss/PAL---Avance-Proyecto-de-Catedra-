b81f57d9c35670019d119f4b87431584c03850f6
carlosLGM <100099447+carlosLGM@users.noreply.github.com>
2022-03-28 19:20:00 -0600

    Update avance.cpp

--- avance.cpp
+++ avance.cpp
@@ -4,13 +4,42 @@ void smsBienvenida();
 
 int main () {
     smsBienvenida();
-    cout<<"1) \n";
-    cout<<"2) \n";
-    cout<<"3) \n";
-    cout<<"4) \n";
-    cout<<"5) \n";
+    cout<<"1)Sucursal \n";
+    cout<<"2)Proveedores \n";
+    cout<<"3)Artículos \n";
+    cout<<"4)Movimientos artículos \n";
+    cout<<"5)Reportes \n";
     cout<<"6) \n";
+
+    int menu;
+     
+	 cout<<"Ingrese un numero del menu"<<endl;
+	 cin>>menu;
+
+        switch (menu){
+           case 1:
+             cout<<"Has seleccionado Sucursal";
+        break;
+            case 2:
+            cout<<"Has seleccionado Proveedores";
+        break;
+            case 3:
+            cout<<"Has seleccionado Artículos";
+        break;
+            case 4:
+            cout<<"Has seleccionado Movimientos artículados";
+        break;
+            case 5:
+           cout<<"Has seleccionado Reportes";
+        break;
+    
+           default:
+           	cout<<"Ingrese una opcion correcta";
+        break;
+        }
 }
+
+
 
 void smsBienvenida () {
     cout<<"\t\t Sistema de Control de Inventarios\n";
