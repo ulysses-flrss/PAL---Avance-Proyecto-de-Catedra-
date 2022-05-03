1bd72f4e7a1e81d226d83f4e84bfd48145cd7dd4
ulysses-flrss <ulisesf3136@gmail.com>
2022-05-03 15:30:20 -0600

    finish change_password

--- PROYECTO_FINAL_PAL.cpp
+++ PROYECTO_FINAL_PAL.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 
 
-int main ()  {
-	setlocale (LC_ALL, "spanish");
-	menu_login();
+int main()
+{
+	setlocale(LC_ALL, "spanish");
+	
+	struct Usuario admin;
+	strcpy(admin.username, "admin");
+	strcpy(admin.password, "1234");
+
+	struct Usuario supervisor;
+	strcpy(supervisor.username, "super");
+	strcpy(supervisor.password, "4321");
+
+	menu_login(admin, supervisor);
 	return 0;
 }
-
-	
-
--- inc/functions.h
+++ inc/functions.h
@@ -2,73 +2,85 @@
 #include <cstring>
 #include <conio.h>
 using namespace std;
+//0303 = ñ
+//0301 = á
 
-struct Usuario {
+struct Usuario
+{
 	char username[15];
 	char password[15];
 };
 
 // DECLARACION DE FUNCIONES
 void welcome();
-void menu_login(struct Usuario &admin, struct Usuario &supervisor);
-void login(struct Usuario &admin, struct Usuario &supervisor);
-void validarUsuario(char userBuff[15], char passBuff[15]);
+void menu_login(struct Usuario admin, struct Usuario supervisor);
+void login(struct Usuario admin, struct Usuario supervisor);
+void validarUsuario(char userBuff[15], char passBuff[15], struct Usuario admin, struct Usuario supervisor);
 void menuAdmin();
 void menuSupervisor();
 void validacionOpciones(int opcionElegida, int primeraOpcion, int ultimaOpcion);
 void menuAdmin2(int opcion);
 void subMenuAdmin(int opcion, char area[25]);
-void changePassword (int cargo, struct Usuario &admin, struct Usuario &supervisor);
-void recoverPassword (int cargo, 0, );
+void changePassword(int cargo, struct Usuario admin, struct Usuario supervisor);
+void recoverPassword(int cargo, struct Usuario admin, struct Usuario supervisor);
 
-void welcome() {
+void welcome()
+{
 	cout << "MENSAJE DE EJEMPLO\n";
 }
 
-void menu_login(struct Usuario &admin, struct Usuario &supervisor) {
+void menu_login(struct Usuario admin, struct Usuario supervisor)
+{
 	int opc = 0;
 
-	do {
-	cout<<"\nQué desea hacer?\n";
-	cout<<"1- Iniciar sesión\n";
-	cout<<"2- Cambiar Contraseña\n";
-	cout<<"3- Recuperar Contraseña\n";
-	cout<<"4- Salir\n";
-	cin>>opc;
-
-	if (!(opc)) {
-		validacionOpciones(opc, 1, 6);
-	} else {
-		validacionOpciones(opc, 1, 6);
-	}
-
-	switch (opc)
+	do
 	{
-	case 1:
-		login(admin, supervisor);
-		break;
+		cout << "\nQue\u0301 desea hacer?\n";
+		cout << "1- Iniciar sesio\u0301n\n";
+		cout << "2- Cambiar Contrasen\u0303a\n";
+		cout << "3- Recuperar Contrasen\u0303a\n";
+		cout << "4- Salir\n";
+		cin >> opc;
 
-	case 2:
-		changePassword(0, admin, supervisor);
-		break;
+		if (!(opc))
+		{
+			validacionOpciones(opc, 1, 6);
+		}
+		else
+		{
+			validacionOpciones(opc, 1, 6);
+		}
 
-	case 3:
-		recoverPassword(0, admin, supervisor);
-		break;
+		switch (opc)
+		{
+		case 1:
+			login(admin, supervisor);
+			break;
 
-	case 4:
-		break;
-	
-	default:
+		case 2:
+			changePassword(0, admin, supervisor);
+			break;
 
-		break;
-	}
+		case 3:
+			recoverPassword(0, admin, supervisor);
+			break;
+
+		case 4:
+			break;
+
+		default:
+			cout<<"OPCIO\u0303N INVA\u0303LIDA...";
+			cin.ignore();
+			getch();
+			system("cls");
+			system("clear");
+			break;
+		}
 	} while (opc < 1 || opc > 6 || opc != 6);
+}
 
-
-	} 
-
-void login(struct Usuario &admin, struct Usuario &supervisor) {
+void login(struct Usuario admin, struct Usuario supervisor)
+{
 	char userBuff[15];
 
 	char passBuff[15];
@@ -76,59 +88,58 @@ void login(struct Usuario &admin, struct Usuario &supervisor) {
 	cout << "Ingrese su nombre de usuario: \n";
 	cin >> userBuff;
 
-	cout << "Ingrese su contrase\06a: \n";
+	cout << "Ingrese su contrasen\u0303a: \n";
 	cin >> passBuff;
 
-	validarUsuario(userBuff, passBuff);
+	validarUsuario(userBuff, passBuff, admin, supervisor);
 }
 
-
-void validarUsuario(char userBuff[15], char passBuff[15]) {
-
-		Usuario admin;
-		strcpy(admin.username, "admin");
-		strcpy(admin.password, "1234");
-
-		Usuario supervisor;
-		strcpy(supervisor.username, "super");
-		strcpy(supervisor.password, "4321");
-	
-
+void validarUsuario(char userBuff[15], char passBuff[15], struct Usuario admin, struct Usuario supervisor)
+{
 	if (strcmp(admin.username, userBuff) == 0 &&
-		strcmp(admin.password, passBuff) == 0) {
+			strcmp(admin.password, passBuff) == 0)
+	{
 		menuAdmin();
-
-	} else if (
-		strcmp(supervisor.username, userBuff) == 0 &&
-		strcmp(supervisor.password, passBuff) == 0) {
+	}
+	else if (
+			strcmp(supervisor.username, userBuff) == 0 &&
+			strcmp(supervisor.password, passBuff) == 0)
+	{
 		menuSupervisor();
-	} else {
+	}
+	else
+	{
 		cout << "ERROR: Nombre o Usuario Incorrectos. Presione ENTER e "
-				"Intentelo de Nuevo \n";
+						"Inte\u0301ntelo de Nuevo \n";
 		cin.ignore();
 		getch();
-		login();
+		login(admin, supervisor);
 	};
 }
 
 // MENU ADMINISTRADOR
-void menuAdmin() {
+void menuAdmin()
+{
 	int menuOpc = 0;
 
-	do {
+	do
+	{
 		welcome();
 		cout << "1)   Sucursal \n";
 		cout << "2)   Proveedores \n";
-		cout << "3)   Art\241culos \n";
-		cout << "4)   Movimientos art�culos \n";
+		cout << "3)   Arti\u0301culos \n";
+		cout << "4)   Movimientos arti\u0301culos \n";
 		cout << "5)   Reportes \n";
 		cout << "6)   Salir\n";
-		cout << "Seleccione la opci�n requerida: \n";
+		cout << "Seleccione la opcion\u0301n requerida: \n";
 		cin >> menuOpc;
 
-		if (!(menuOpc)) {
+		if (!(menuOpc))
+		{
 			validacionOpciones(menuOpc, 1, 6);
-		} else {
+		}
+		else
+		{
 			validacionOpciones(menuOpc, 1, 6);
 			menuAdmin2(menuOpc);
 		}
@@ -136,14 +147,16 @@ void menuAdmin() {
 	} while (menuOpc < 1 || menuOpc > 6 || menuOpc != 6);
 }
 
-void menuAdmin2(int opcion) {
+void menuAdmin2(int opcion)
+{
 	char titulo[5][25] = {"Sucursal",
-						  "Proveedor",
-						  "Articulo",
-						  "Movimientos Art�culos",
-						  "Reporte"};
+												"Proveedor",
+												"Arti\u0301culo",
+												"Movimientos Arti\u0301culos",
+												"Reporte"};
 
-	switch (opcion) {
+	switch (opcion)
+	{
 	case 1:
 		subMenuAdmin(1, titulo[0]);
 		break;
@@ -175,17 +188,16 @@ void menuAdmin2(int opcion) {
 	}
 }
 
-void subMenuAdmin(int opcion, char area[25]) {
+void subMenuAdmin(int opcion, char area[25])
+{
 	int subMenu = 0;
 	switch (opcion) {
-	case 1:
-	case 2:
-	case 3:
+	case 1: case 2: case 3:
 		cout << "Ingresado a " << area << endl;
 		cout << "1) Registrar " << area << endl;
 		cout << "2) Modificar " << area << endl;
 		cout << "3) Ver registro de " << area << "s\n";
-		cout << "4) Regresar al men�\n";
+		cout << "4) Regresar al menu\u0301\n";
 		cin >> subMenu;
 
 		if (!(subMenu)) {
@@ -204,9 +216,12 @@ void subMenuAdmin(int opcion, char area[25]) {
 		cout << "2) Registro de salida de inventarios\n";
 		cin >> subMenu;
 		validacionOpciones(subMenu, 1, 2);
-		if (!(subMenu)) {
+		if (!(subMenu))
+		{
 			validacionOpciones(subMenu, 1, 4);
-		} else {
+		}
+		else
+		{
 			// subMenu2(subMenu);
 		}
 		break;
@@ -218,9 +233,12 @@ void subMenuAdmin(int opcion, char area[25]) {
 		cout << "2) Reporte de Movimientos\n";
 		cin >> subMenu;
 		validacionOpciones(subMenu, 1, 2);
-		if (!(subMenu)) {
+		if (!(subMenu))
+		{
 			validacionOpciones(subMenu, 1, 4);
-		} else {
+		}
+		else
+		{
 			// subMenu2(subMenu);
 		}
 		break;
@@ -230,27 +248,32 @@ void subMenuAdmin(int opcion, char area[25]) {
 	}
 }
 
-void menuSupervisor() {
+void menuSupervisor()
+{
 }
 
 void validacionOpciones(
-	int opcionElegida,
-	int primeraOpcion,
-	int ultimaOpcion) {
-	if (!(opcionElegida)) {
+		int opcionElegida,
+		int primeraOpcion,
+		int ultimaOpcion)
+{
+	if (!(opcionElegida))
+	{
 		cin.clear();
 		cin.ignore();
-		cout << "ERROR: OPCI�N INV�LIDA \n";
-		cout << "Presione ENTER e int�ntelo de Nuevo...\n";
+		cout << "ERROR: OPCIO\u0301N INVA\u0301LIDA \n";
+		cout << "Presione ENTER e inte\u0301ntelo de Nuevo...\n";
 		cin.ignore();
 		getch();
 		system("clear");
 		system("cls");
-	} else if (opcionElegida < primeraOpcion || opcionElegida > ultimaOpcion) {
+	}
+	else if (opcionElegida < primeraOpcion || opcionElegida > ultimaOpcion)
+	{
 		cin.clear();
 		cin.ignore();
-		cout << "ERROR: OPCI�N INV�LIDA \n";
-		cout << "Presione ENTER e int�ntelo de Nuevo...\n";
+		cout << "ERROR: OPCIO\u0301N INVA\u0301LIDA \n";
+		cout << "Presione ENTER e inte\u0301ntelo de Nuevo...\n";
 
 		cin.ignore();
 		getch();
@@ -260,88 +283,94 @@ void validacionOpciones(
 	// HASTA AQUI-----
 }
 
-void changePassword(int cargo, struct Usuario &admin, struct Usuario &supervisor){
+void changePassword(int cargo, struct Usuario admin, struct Usuario supervisor)
+{
 	char password[15];
 
-	if (cargo == 0) {
+	if (cargo == 0)
+	{
 		strcpy(password, admin.password);
-	} else if (cargo == 1) {
+	}
+	else if (cargo == 1)
+	{
 		strcpy(password, supervisor.password);
 	}
-
 
 	char currentPassword[15];
 	char user[15];
 
 	int intento = 2;
-	do {
+	do
+	{
 
+		cout << "A continuacio\u0301n procedera\u0301 a cambiar su contrasen\u0303a\n";
+		cout << "Ingrese su usuario:\n";
+		cin >> user;
 
-		cout<<"A continuación procederá a cambiar su contraseña\n";
-		cout<<"Ingrese su usuario\n";
-		cin>>user;
-
-		if (strcmp(user, admin.username) == 0) {
-			cout<<"Ingrese su contraseña actual\n";
-			cin>>currentPassword;
-			if (strcmp(currentPassword, admin.password) == 0){
-				cout<<"Ingrese su nueva contraseña\n";
-				cin>>admin.password;
-				cout<<"CONTRASEÑA VALIDA";
-				login();
-			} else {
-				cout<<"Contraseña incorrecta...Le restan "<<intento<<" Intentos\n";
+		if (strcmp(user, admin.username) == 0)
+		{
+			cout << "Ingrese su contrasen\u0303a actual\n";
+			cin >> currentPassword;
+			if (strcmp(currentPassword, admin.password) == 0)
+			{
+				cout << "Ingrese su nueva contrasen\u0303a\n";
+				cin >> admin.password;
+				cout << "CONTRASEN\u0303A VALIDA";
+				menu_login(admin, supervisor);
 			}
-		} else if (strcmp(user, supervisor.username) == 0) {
-			cout<<"Ingrese su contraseña actual\n";
-			cin>>currentPassword;
-			if (strcmp(currentPassword, admin.password) == 0){
-				cout<<"Ingrese su nueva contraseña\n";
-				cin>>admin.password;
-				cout<<"CONTRASEÑA VALIDA";
-				login();
-			} else {
-				cout<<"Contraseña incorrecta...Le restan "<<intento<<" Intentos\n";
+			else
+			{
+				cout << "Contrasen\u0303a incorrecta...Le restan " << intento << " Intentos\n";
 			}
-		} else {
-			cout<<"Usuario invalido. Le restan "<<intento<<" Intentos";
-
+		}
+		else if (strcmp(user, supervisor.username) == 0)
+		{
+			cout << "Ingrese su contrasen\u0303a actual\n";
+			cin >> currentPassword;
+			if (strcmp(currentPassword, admin.password) == 0)
+			{
+				cout << "Ingrese su nueva contraseÃ±a\n";
+				cin >> admin.password;
+				cout << "CONTRASEN\u0303A VALIDA";
+				login(admin, supervisor);
+			}
+			else
+			{
+				cout << "Contrasen\u0303a incorrecta...Le restan " << intento << " Intentos\n";
+			}
+		}
+		else
+		{
+			cout << "Usuario invalido. Le restan " << intento << " Intentos";
 		}
 
 		intento--;
 	} while (intento >= 0);
 }
 
+void recoverPassword(int cargo,struct Usuario admin, struct Usuario supervisor)
+{
+	char password[15];
 
-void recoverPassword(int cargo){
-
-	Usuario admin;
-	strcpy(admin.username, "admin");
-	strcpy(admin.password, "1234");
-
-	Usuario supervisor;
-	strcpy(supervisor.username, "super");
-	strcpy(supervisor.password, "4321");
-	
-
-char password[15];
-
-	if (cargo == 0) {
+	if (cargo == 0)
+	{
 		strcpy(password, admin.password);
-	} else if (cargo == 1) {
+	}
+	else if (cargo == 1)
+	{
 		strcpy(password, supervisor.password);
 	}
 
-
 	char currentPassword[15];
 
-	cout<<"A continuación procederá a cambiar su contraseña";
-	cout<<"Ingrese su contraseña actual";
-	cin>>currentPassword;
+	cout << "A continuacio\u0301n procedera\u0301 a cambiar su contrasen\u0303a";
+	cout << "Ingrese su contrase\u0303a actual";
+	cin >> currentPassword;
 
-	if (strcmp(password, currentPassword) == 0) {
+	if (strcmp(password, currentPassword) == 0)
+	{
 		char new_password[15];
-		cout<<"Escriba su nueva contraseña";
-		cin>>new_password;
+		cout << "Escriba su nueva contrase\u0303a";
+		cin >> new_password;
 	}
 }
